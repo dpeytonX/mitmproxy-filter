b03b51c4fa831d84802705409a41f03984314f19
prplmnky <prplmnky@localhost>
2019-06-04 22:08:33 -0400

    better logging for grep, fixed ip host lookup

--- filterads.py
+++ filterads.py
@@ -10,10 +10,14 @@ class Filter:
     def __init__(self):
         self.sites = [line.rstrip('\n') for line in open('filters.txt')]
 
-        #for s in self.sites:
-        #    ip = self.getIp(s.replace('\\',''))
-        #    if(ip is not None):
-        #        self.sites.extend([ip])
+        for s in self.sites:
+            if(s.count('.') != 3 or s.count(':') > 0):
+                ip = self.getIp(s.replace('\\',''))
+                if(ip is not None and len(ip) > 0):
+                    self.sites.extend(ip)
+                    print("PASS: found %s for %s" % (ip, s))
+                else:
+                    print("PASS: did not find IP for %s" % s)
 
         self.blocked = {}
         self.allowed = {}
@@ -26,8 +30,7 @@ class Filter:
 
         if(server is not None):
             if(not self.checkSite(server)):
-                ctx.log.info("Killing flow %s" % server)
-                print(server_conn.address)
+                ctx.log.info("PASS: Killing flow %s" % server)
                 server_conn.address = ("", server_conn.address[1])
                 #raise exceptions.Kill()
 
@@ -35,9 +38,9 @@ class Filter:
             try:
                 lookup = socket.getaddrinfo(host,443)
                 if(lookup is not None and len(lookup) > 4):
-                    return lookup[4][1];
+                    return [i[4][0] for i in lookup];
             except socket.gaierror as e:
-                print("%s : %s" % (e, host))
+                ctx.log.info("PASS: %s : %s" % (e, host))
             return None
 
 
@@ -47,25 +50,29 @@ class Filter:
                 if(lookup is not None and len(lookup) > 0):
                     return lookup[0];
             except socket.herror as e:
-                print("%s : %s" % (e, ip))
-            return ip
+                ctx.log.info("PASS: %s : %s" % (e, ip))
+            return None
         
 
     def checkSite(self, site):
-        if(site in self.blocked):
+        host = self.getHost(site)
+        if(host is None):
+            host = site
+
+        if(site in self.blocked or host in self.blocked):
             return False
 
-        if(site not in self.allowed):
-            host = self.getHost(site)
-
+        if(site not in self.allowed and host not in self.allowed):
             for line in self.sites:
                 p = re.compile(line, re.IGNORECASE)
 
-                if(p.search(host) is not None):
-                    ctx.log.info("server %s matched filter %s" % (host, line))
+                if(p.search(host) is not None or p.search(site) is not None):
+                    ctx.log.info("PASS: server %s,%s matched filter %s" % (host, site, line))
                     self.blocked[site] = True
+                    self.blocked[host] = True
                     return False
             self.allowed[site] = True
+            self.allowed[host] = True
 
         return True
 
